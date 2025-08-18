0684959c6e81cfc70b3a17f869fd421403fdc495
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-08-18 16:48:14 +0300

    Görev Ekle ekranı yapıldı!

--- ToDoApp/Controllers/MissionController.cs
+++ ToDoApp/Controllers/MissionController.cs
@@ -14,7 +14,7 @@ namespace ToDoApp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            var degerler = db.Missions.Select(m => new MissionViewModel
+            var degerler = db.Missions.OrderByDescending(x=>x.Importance).Select(m => new MissionViewModel
             {
                 MissionId = m.MissionId,
                 MissionTitle = m.Title,
@@ -30,6 +30,53 @@ namespace ToDoApp.Controllers
 
             return View(degerler);
         }
+        [HttpGet]
+        public IActionResult AddMission()
+        {
+            var kurumlar = db.Organisations.Where(x=>x.IsActive==true).ToList();
 
+            ViewBag.Kurumlar = kurumlar;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMission(MissionAddViewModel missionAddViewModel)
+        {
+            int ImportanceCode = 0;
+
+            if (missionAddViewModel.ImportanceStatus == true && missionAddViewModel.UrgencyStatus == true) { ImportanceCode = 4; }
+            else if (missionAddViewModel.ImportanceStatus == false && missionAddViewModel.UrgencyStatus == true) { ImportanceCode = 3; }
+            else if (missionAddViewModel.ImportanceStatus == true && missionAddViewModel.UrgencyStatus == false) { ImportanceCode = 2; }
+            else if (missionAddViewModel.ImportanceStatus == false && missionAddViewModel.UrgencyStatus == false) { ImportanceCode = 1; }
+            Mission mission = new Mission
+            {
+                Title = missionAddViewModel.Title,
+                Importance = ImportanceCode,
+                Organisation = db.Organisations.Find(missionAddViewModel.OrganisationId),
+                Steps = new List<Step>()
+            };
+            for(int i=1;i < (missionAddViewModel.Steps+1); i++)
+            {
+                Step step = new Step
+                {
+                    StepName = i.ToString(),
+                    IsCompleted = false,
+                    Mission = mission
+                };
+                db.Steps.Add(step);
+            }
+            db.Missions.Add(mission);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateMission(int id)
+        {
+            var mission = db.Missions.Find(id);
+            if (mission == null)
+            {
+                return NotFound();
+            }
+            return View(mission);
+        }
     }
 }
---
+++ ToDoApp/Models/MissionAddViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ToDoApp.DAL.Entities;
+
+namespace ToDoApp.Models
+{
+    public class MissionAddViewModel
+    {
+        public string Title { get; set; }
+        public bool ImportanceStatus { get; set; }
+        public bool UrgencyStatus { get; set; }
+        public int OrganisationId { get; set; }
+        public int Steps { get; set; }
+    }
+}
